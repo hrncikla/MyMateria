f1e8210e321a3a590ce25b60d592e47939a5b345
Klára Hrnčířová <hrncirova.clara@gmail.com>
2025-05-14 18:23:06 +0200

    new: version

---
+++ assets/Download-DgU-ysML.js
@@ -0,0 +1,47 @@
+import{j as e,M as s}from"./index-atSoaKhH.js";import{useMDXComponents as r}from"./index-BKthzpKn.js";import"./iframe-CnA8-RDm.js";import"./index-CfOrKyLd.js";import"./index-DrFu-skq.js";function i(t){const n={a:"a",code:"code",h1:"h1",h2:"h2",h3:"h3",hr:"hr",p:"p",pre:"pre",...r(),...t.components};return e.jsxs(e.Fragment,{children:[e.jsx(s,{title:"Download & Setup"}),`
+`,e.jsx(n.h1,{id:"download--setup",children:"Download & Setup"}),`
+`,e.jsx(n.p,{children:"You can get started by cloning the full repository or by using the pre-built minified CSS directly from GitHub Pages."}),`
+`,e.jsx(n.h3,{id:"use-the-minified-css-quickest",children:"Use the Minified CSS (quickest)"}),`
+`,e.jsx(n.p,{children:"You can include the pre-built, minified CSS directly from GitHub Pages:"}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{className:"language-html",children:`<link rel="stylesheet" href="https://hrncikla.github.io/CSS-Materia/css/materia.min.css" />
+`})}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{className:"language-html",children:`<!DOCTYPE html>\r
+<html lang="en">\r
+  <head>\r
+    <meta charset="UTF-8" />\r
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />\r
+    <title>Title</title>\r
+    <link rel="stylesheet" href="https://hrncikla.github.io/CSS-Materia/css/materia.min.css" />\r
+  </head>\r
+  <body>\r
+  </body>\r
+</html>
+`})}),`
+`,e.jsx(n.p,{children:"This will include the latest version of My Materia without the need to clone the entire repository."}),`
+`,e.jsx(n.p,{children:"You can get started by cloning the full repository or downloading the source files manually."}),`
+`,e.jsx(n.h3,{id:"clone-the-repository",children:"Clone the repository"}),`
+`,e.jsx(n.p,{children:e.jsx(n.a,{href:"https://github.com/hrncikla/CSS-Materia",rel:"nofollow",children:"GitHub Repository"})}),`
+`,e.jsx(n.p,{children:"To clone the latest version of My Materia from GitHub, run the following command in your terminal:"}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{className:"language-bash",children:`git clone https://github.com/hrncikla/CSS-Materia.git
+`})}),`
+`,e.jsx(n.h3,{id:"directory-structure",children:"Directory Structure"}),`
+`,e.jsx(n.p,{children:"This will create a local copy of the project with the following structure:"}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{children:`CSS-Materia/\r
+├── .storybook/      # Storybook configuration\r
+├── build/           # Compiled and minified CSS/JS files\r
+├── public/          # Static files for deployment\r
+├── src/             # Source files (SCSS, JS, components)\r
+├── .gitignore       # Git ignore rules\r
+├── LICENSE          # License file\r
+├── package-lock.json # Dependency lock file\r
+├── package.json     # Project dependencies and scripts\r
+└── README.md        # Project overview and setup instructions
+`})}),`
+`,e.jsx(n.h3,{id:"install-dependencies",children:"Install Dependencies"}),`
+`,e.jsx(n.p,{children:"After cloning, navigate into the project directory and install the required packages:"}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{className:"language-bash",children:`cd CSS-Materia\r
+npm install
+`})}),`
+`,e.jsx(n.hr,{}),`
+`,e.jsx(n.h2,{id:"2-next-steps",children:"2. Next Steps"}),`
+`,e.jsxs(n.p,{children:["To continue, visit the ",e.jsx(n.a,{href:"./?path=/docs/get-started--docs",children:"Get Started"})," page for further instructions on using My Materia in your project."]})]})}function d(t={}){const{wrapper:n}={...r(),...t.components};return n?e.jsx(n,{...t,children:e.jsx(i,{...t})}):i(t)}export{d as default};
